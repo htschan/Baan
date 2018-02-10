824199ec0786a427f0247bef7b34da9e6f2928c7
Hans Tschan <Hans.Tschan@gmail.com>
2018-02-10 20:53:52 +0100

    removed cache control and splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { Platform, ModalController } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Platform } from 'ionic-angular';
 
 import { TabsPage } from '../pages/tabs/tabs';
-import { SplashPage } from '../pages/splash/splash';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,17 +9,12 @@ import { SplashPage } from '../pages/splash/splash';
 export class MyApp {
   rootPage: any = null;
 
-  constructor(platform: Platform, statusBar: StatusBar, modalCtrl: ModalController) {
+  constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       // statusBar.styleDefault();
-      let splash = modalCtrl.create(SplashPage);
-      splash.present();
-
-      setTimeout(() => {
         this.rootPage = TabsPage;
-      }, 1000);
     });
   }
 }
