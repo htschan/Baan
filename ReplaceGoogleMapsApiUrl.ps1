37de6909efb964269ebcadd783bb623942c7500d
Hans Tschan <Hans.Tschan@gmail.com>
2018-03-30 17:46:54 +0200

    fixed typo

--- ReplaceGoogleMapsApiUrl.ps1
+++ ReplaceGoogleMapsApiUrl.ps1
@@ -2,7 +2,7 @@ Param(
     [Parameter(Mandatory=$true)]
     [string]$Workspace,
     [Parameter(Mandatory=$true)]
-    [string]$ProjectConfigFolder,
+    [string]$ProjectConfigFolder
 )
 $GoogleMapsApiUrl = Get-Content $ProjectConfigFolder/GoogleMapsApiUrl.txt
 (Get-Content $Workspace/src/index.html) -replace "https://maps.googleapis.com/maps/api/js", "$GoogleMapsApiUrl" | Set-Content $Workspace/src/index.html
