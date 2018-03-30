c755dff5e9becb240a204ff856a02cd3feee1c08
Hans Tschan <Hans.Tschan@gmail.com>
2018-03-30 17:45:12 +0200

    Jenkins add Google Maps Api Key

---
+++ ReplaceGoogleMapsApiUrl.ps1
@@ -0,0 +1,8 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$Workspace,
+    [Parameter(Mandatory=$true)]
+    [string]$ProjectConfigFolder,
+)
+$GoogleMapsApiUrl = Get-Content $ProjectConfigFolder/GoogleMapsApiUrl.txt
+(Get-Content $Workspace/src/index.html) -replace "https://maps.googleapis.com/maps/api/js", "$GoogleMapsApiUrl" | Set-Content $Workspace/src/index.html
