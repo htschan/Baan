8f8317bfbc9ba86e807176b050328f9bebe86fc2
Hans Tschan <Hans.Tschan@gmail.com>
2018-04-04 19:36:51 +0200

    chat in progress

--- src/pages/chat-chat/chat-chat.ts
+++ src/pages/chat-chat/chat-chat.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Content } from 'ionic-angular';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 const FbBase = "/MyHome";
 
@@ -12,11 +13,11 @@ const FbBase = "/MyHome";
 export class ChatChatPage {
   @ViewChild(Content) content: Content;
 
-  data = { type:'', nickname:'', message:'' };
-  chats = [];
-  roomkey:string;
-  nickname:string;
-  offStatus:boolean = false;
+  data = { type: '', nickname: '', message: '' };
+  chats: Observable<any[]>;
+  roomkey: string;
+  nickname: string;
+  offStatus: boolean = false;
   ref: AngularFireList<any>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFireDatabase) {
@@ -25,71 +26,45 @@ export class ChatChatPage {
     this.data.type = 'message';
     this.data.nickname = this.nickname;
     this.ref = af.list(`${FbBase}/Chatrooms/`);
-/*
-    let joinData = this.af.firebase.database().ref('Chatrooms/'+this.roomkey+'/chats').push();
+
+    let joinData = af.list(`${FbBase}/Chatrooms/${this.roomkey}/chats`).push({});
     joinData.set({
-      type:'join',
-      user:this.nickname,
-      message:this.nickname+' has joined this room.',
-      sendDate:Date()
+      type: 'join',
+      user: this.nickname,
+      message: this.nickname + ' has joined this room.',
+      sendDate: Date()
     });
-    */
+
     this.data.message = '';
-/*
-    firebase.database().ref('Chatrooms/'+this.roomkey+'/chats').on('value', resp => {
-      this.chats = [];
-      this.chats = snapshotToArray(resp);
-      setTimeout(() => {
-        if(this.offStatus === false) {
-          this.content.scrollToBottom(300);
-        }
-      }, 1000);
+
+    this.chats = af.list(`${FbBase}/Chatrooms/${this.roomkey}/chats`).snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
     });
-    */
   }
 
   sendMessage() {
-    /*
-    let newData = firebase.database().ref('Chatrooms/'+this.roomkey+'/chats').push();
+    let newData = this.af.list(`${FbBase}/Chatrooms/${this.roomkey}/chats`).push({});
     newData.set({
-      type:this.data.type,
-      user:this.data.nickname,
-      message:this.data.message,
-      sendDate:Date()
+      type: this.data.type,
+      user: this.data.nickname,
+      message: this.data.message,
+      sendDate: Date()
     });
-    */
     this.data.message = '';
   }
 
   exitChat() {
-    /*
-    let exitData = firebase.database().ref('Chatrooms/'+this.roomkey+'/chats').push();
+    let exitData = this.af.list(`${FbBase}/Chatrooms/${this.roomkey}/chats`).push({});
     exitData.set({
-      type:'exit',
-      user:this.nickname,
-      message:this.nickname+' has exited this room.',
-      sendDate:Date()
+      type: 'exit',
+      user: this.nickname,
+      message: this.nickname + ' has exited this room.',
+      sendDate: Date()
     });
-*/
     this.offStatus = true;
 
     this.navCtrl.setRoot("ChatRoomPage", {
-      nickname:this.nickname
+      nickname: this.nickname
     });
   }
-
-
 }
-
-
-export const snapshotToArray = snapshot => {
-  let returnArr = [];
-
-  snapshot.forEach(childSnapshot => {
-      let item = childSnapshot.val();
-      item.key = childSnapshot.key;
-      returnArr.push(item);
-  });
-
-  return returnArr;
-};
