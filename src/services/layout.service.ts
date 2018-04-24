6a85be06656586e5fad3ded9d21b99c2706bdfd2
Hans Tschan <Hans.Tschan@gmail.com>
2018-04-24 20:04:02 +0200

    added device info on about page

--- src/services/layout.service.ts
+++ src/services/layout.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Platform } from "ionic-angular/platform/platform";
 
 
 
@@ -8,10 +9,26 @@ export class LayoutService {
 
     data: BehaviorSubject<ScreenProperties> = new BehaviorSubject<ScreenProperties>({ width: 100, height: 100 });
 
-    constructor() { }
+    constructor(private plt: Platform) { }
 
     setScreenSize(width: number, height: number) {
         this.data.next({ width: width, height: height });
+    }
+
+    isPlatform(name: string) {
+        return this.plt.is(name);
+    }
+
+    getPlatforms() {
+        return this.plt.platforms();
+    }
+
+    isPortrait() {
+        return this.plt.isPortrait();
+    }
+
+    isLandscape() {
+        return this.plt.isLandscape();
     }
 }
 
