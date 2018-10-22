f04c9bc696e3804d94dffbae5a37e42c0e04a310
Hans Tschan <Hans.Tschan@gmail.com>
2018-10-22 20:09:40 +0200

    Fix slide navigation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { ModalController, Events, PopoverController, App } from '@ionic/angular';
+import { Events, PopoverController, App } from '@ionic/angular';
 import { LayoutService } from '../services/layout.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -17,12 +18,11 @@ export class HomePage implements OnInit, OnDestroy {
 
   constructor(
     public auth: AuthService,
+    private router: Router,
     public appCtrl: App,
-    public modalCtrl: ModalController,
     public events: Events,
     public layoutService: LayoutService,
     public popoverCtrl: PopoverController) {
-    // super(auth, modalCtrl, appCtrl);
     events.subscribe('user:signout', (user) => {
       console.log('home.component user logged out');
     });
@@ -32,19 +32,19 @@ export class HomePage implements OnInit, OnDestroy {
     });
     console.dir(AuthService);
     this.tiles = [
-      { title: 'Einkaufen', imgsrc: '../../assets/imgs/mm_shop.png', pgname: 'ShoppingPage' },
-      { title: 'Todo', imgsrc: '../../assets/imgs/mm_todo.png', pgname: 'TodoPage' },
-      { title: 'Audio Tracks', imgsrc: '../../assets/imgs/mm_tracks.png', pgname: 'SonglistPage' },
-      { title: 'Youtube Download', imgsrc: '../../assets/imgs/mm_tube.png', pgname: 'YoutubedownloadPage' },
-      { title: 'Kamera', imgsrc: '../../assets/imgs/mm_camera.png', pgname: 'CameraPage' },
-      { title: 'Test', imgsrc: '../../assets/imgs/mm_test.png', pgname: 'TestPage' },
-      { title: 'GPS', imgsrc: '../../assets/imgs/mm_gps.png', pgname: 'GpsPage' },
-      { title: 'KML', imgsrc: '../../assets/imgs/mm_gps.png', pgname: 'KmlPage' },
-      { title: 'Motion', imgsrc: '../../assets/imgs/mm_motion.png', pgname: 'MotionPage' },
-      { title: 'Chat', imgsrc: '../../assets/imgs/mm_chat.png', pgname: 'ChatRoomPage' },
-      { title: 'Profil', imgsrc: '../../assets/imgs/mm_about.png', pgname: 'ProfilePage' },
-      { title: 'Über', imgsrc: '../../assets/imgs/mm_about.png', pgname: 'AboutPage' },
-      { title: 'Kontakt', imgsrc: '../../assets/imgs/mm_contact.png', pgname: 'ContactPage' },
+      { title: 'Einkaufen', imgsrc: '../../assets/imgs/mm_shop.png', pgname: 'Shopping' },
+      { title: 'Todo', imgsrc: '../../assets/imgs/mm_todo.png', pgname: 'Todo' },
+      { title: 'Audio Tracks', imgsrc: '../../assets/imgs/mm_tracks.png', pgname: 'Songlist' },
+      { title: 'Youtube Download', imgsrc: '../../assets/imgs/mm_tube.png', pgname: 'Youtubedownload' },
+      { title: 'Kamera', imgsrc: '../../assets/imgs/mm_camera.png', pgname: 'Camera' },
+      { title: 'Test', imgsrc: '../../assets/imgs/mm_test.png', pgname: 'Test' },
+      { title: 'GPS', imgsrc: '../../assets/imgs/mm_gps.png', pgname: 'Gps' },
+      { title: 'KML', imgsrc: '../../assets/imgs/mm_gps.png', pgname: 'Kml' },
+      { title: 'Motion', imgsrc: '../../assets/imgs/mm_motion.png', pgname: 'Motion' },
+      { title: 'Chat', imgsrc: '../../assets/imgs/mm_chat.png', pgname: 'ChatRoom' },
+      { title: 'Profil', imgsrc: '../../assets/imgs/mm_about.png', pgname: 'Profil' },
+      { title: 'Über', imgsrc: '../../assets/imgs/mm_about.png', pgname: 'About' },
+      { title: 'Kontakt', imgsrc: '../../assets/imgs/mm_contact.png', pgname: 'Contact' },
     ];
     this.widthsubscription = layoutService.data.subscribe(data => {
       const colcount = Math.trunc(Math.abs(data.contentWidth - 80) / 120 + 1);
@@ -75,9 +75,6 @@ export class HomePage implements OnInit, OnDestroy {
   }
 
   openPage(page: string) {
-    console.dir(page);
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    // this.navCtrl.setRoot(page);
+    this.router.navigate([`${page}`]);
   }
 }
